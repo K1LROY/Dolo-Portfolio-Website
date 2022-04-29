5aa3ce349e671b26b6e9c77607ee0983062f3eb7
Supatarchai <Luck-y_se7en@hotmail.com>
2022-04-29 16:54:37 +0700

    Add Content About

---
+++ src/components/contents/About.js
@@ -0,0 +1,75 @@
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import WOW from "wowjs";
+
+import "../../styles/about.css";
+
+export default function About() {
+  useEffect(() => {
+    new WOW.WOW({
+      live: false,
+    }).init();
+  }, []);
+
+  return (
+    <div className="about" id="about">
+      <div className="container spaceContent">
+        <div className="row">
+          <div
+            className="col-md-6 wow bounceInLeft"
+            data-wow-duration="1.1s"
+            data-wow-delay="1.1s"
+          >
+            <img src="images/seo-home1.png" alt="" />
+          </div>
+          <div
+            className="col-md-5 text-white gap wow bounceInLeft mt-5"
+            data-wow-duration="1.1s"
+            data-wow-delay="1.1s"
+          >
+            <h2>About me</h2>
+
+            <p className="mt-4">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit,
+              quasi, molestias? Velit similique harum ipsa, unde consectetur
+              cupiditate numquam ipsum error qui. Voluptate excepturi esse modi
+              eaque rerum pariatur, doloribus.
+            </p>
+
+            <div className="tag mt-3">
+              <ul>
+                <li>
+                  <span>HTML</span>
+                </li>
+                <li>
+                  <span>CSS</span>
+                </li>
+                <li>
+                  <span>Javascript</span>
+                </li>
+                <li>
+                  <span>React.js</span>
+                </li>
+                <li>
+                  <span>Next.js</span>
+                </li>
+                <li>
+                  <span>Node.js</span>
+                </li>
+                <li>
+                  <span>Figma</span>
+                </li>
+              </ul>
+            </div>
+
+            <div className="download-button mt-3">
+              <Link to="" className="btn btn-danger">
+                Download cv
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
+import About from "../components/contents/About";
 import Header from "../components/Header";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <About />
     </div>
   );
 }
