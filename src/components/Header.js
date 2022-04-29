a8ac1075d7af3120a2401b634053476335e5a548
Supatarchai <Luck-y_se7en@hotmail.com>
2022-04-29 14:38:18 +0700

    Build SideMenu

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,70 @@ import React from "react";
 import { NavLink, Link } from "react-router-dom";
 import "../styles/header.css";
 
+import $ from "jquery";
+
+$(document).ready(function () {
+  $(".sidemenu-toggler").click(function () {
+    $(".sidemenu").addClass("active");
+  });
+  $(".close").click(function () {
+    $(".sidemenu").removeClass("active");
+  });
+
+  $(window).scroll(function () {
+    var sc = $(window).scrollTop();
+    if (sc > 150) {
+      $(".header").addClass("sticky");
+    } else {
+      $(".header").removeClass("sticky");
+    }
+  });
+});
+
+$(document).ready(function () {
+  $(".sidemenu-toggler").click(function () {
+    $(".sidemenu").addClass("active");
+  });
+
+  $(".close").click(function () {
+    $(".sidemenu").removeClass("active");
+  });
+
+  $(window).scroll(function () {
+    var sc = $(window).scrollTop();
+    if (sc > 150) {
+      $(".header").addClass("sticky");
+    } else {
+      $(".header").removeClass("sticky");
+    }
+  });
+
+  $(window).on("load", function () {
+    var $container = $(".portfolioContainer");
+    $container.isotope({
+      filter: "*",
+      animationOptions: {
+        queue: true,
+      },
+    });
+
+    $(".portfolio-nav li").click(function () {
+      $(".portfolio-nav .current").removeClass("current");
+      $(this).addClass("current");
+      var selector = $(this).attr("data-filter");
+      $container.isotope({
+        filter: selector,
+        animationOptions: {
+          queue: true,
+        },
+      });
+      return false;
+    });
+  });
+
+  $("#portfolio-item").mixItUp();
+});
+
 export default function Header() {
   return (
     <div class="main" id="main">
@@ -53,6 +117,48 @@ export default function Header() {
           <span></span>
           <span></span>
         </Link>
+
+        <div class="sidemenu">
+          <Link to="#" class="close"></Link>
+          <nav>
+            <ul>
+              <li>
+                <NavLink to="">Home</NavLink>
+              </li>
+              <li>
+                <NavLink class="smooth-scroll" to="#about">
+                  About
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="">Pages</NavLink>
+              </li>
+              <li>
+                <NavLink to="">Blog</NavLink>
+              </li>
+              <li>
+                <NavLink to="">Gallery</NavLink>
+              </li>
+              <li>
+                <NavLink to="">Contact</NavLink>
+              </li>
+            </ul>
+          </nav>
+          <footer>
+            <div class="social">
+              <NavLink to="">
+                <img src="images/facebook.png" alt="" />
+              </NavLink>
+              <NavLink to="">
+                <img src="images/twitter.png" alt="" />
+              </NavLink>
+              <NavLink to="">
+                <img src="images/instagram.png" alt="" />
+              </NavLink>
+            </div>
+            <p>2022, Made with love by Supatarachai kwandee</p>
+          </footer>
+        </div>
       </header>
     </div>
   );
