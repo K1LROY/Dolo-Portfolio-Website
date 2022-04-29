a5cf6d9a659d8f9f5dd8a9986ef9e7d68f166129
Supatarchai <Luck-y_se7en@hotmail.com>
2022-04-29 15:23:03 +0700

    Build Profile

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import { NavLink, Link } from "react-router-dom";
 import "../styles/header.css";
 
 import $ from "jquery";
+import Profile from "./contents/Profile";
 
 $(document).ready(function () {
   $(".sidemenu-toggler").click(function () {
@@ -68,98 +69,101 @@ $(document).ready(function () {
 
 export default function Header() {
   return (
-    <div class="main" id="main">
-      <header class="header">
-        <div class="wrap d-flex">
-          <h1 class="logo">
-            <NavLink to="">
-              <img src="images/logo.png" alt="" />
-            </NavLink>
-          </h1>
-          <nav class="menu">
-            <ul>
-              <li>
-                <NavLink class="smooth-scroll" to="#home">
-                  Home
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to="#about" class="nav-link smooth-scroll">
-                  About
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to="#pages" class="smooth-scroll">
-                  Pages
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to="#skills" class="smooth-scroll">
-                  Skills
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to="#gallery" class="smooth-scroll">
-                  Gallery
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to="#mh-contact" class="smooth-scroll">
-                  Contact
-                </NavLink>
-              </li>
-            </ul>
-          </nav>
-        </div>
+    <div>
+      <div class="main" id="main">
+        <header class="header">
+          <div class="wrap d-flex">
+            <h1 class="logo">
+              <NavLink to="">
+                <img src="images/logo.png" alt="" />
+              </NavLink>
+            </h1>
+            <nav class="menu">
+              <ul>
+                <li>
+                  <NavLink class="smooth-scroll" to="#home">
+                    Home
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="#about" class="nav-link smooth-scroll">
+                    About
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="#pages" class="smooth-scroll">
+                    Pages
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="#skills" class="smooth-scroll">
+                    Skills
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="#gallery" class="smooth-scroll">
+                    Gallery
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="#mh-contact" class="smooth-scroll">
+                    Contact
+                  </NavLink>
+                </li>
+              </ul>
+            </nav>
+          </div>
 
-        <Link to="#" class="sidemenu-toggler">
-          <span></span>
-          <span></span>
-          <span></span>
-        </Link>
+          <Link to="#" class="sidemenu-toggler">
+            <span></span>
+            <span></span>
+            <span></span>
+          </Link>
 
-        <div class="sidemenu">
-          <Link to="#" class="close"></Link>
-          <nav>
-            <ul>
-              <li>
-                <NavLink to="">Home</NavLink>
-              </li>
-              <li>
-                <NavLink class="smooth-scroll" to="#about">
-                  About
+          <div class="sidemenu">
+            <Link to="#" class="close"></Link>
+            <nav>
+              <ul>
+                <li>
+                  <NavLink to="">Home</NavLink>
+                </li>
+                <li>
+                  <NavLink class="smooth-scroll" to="#about">
+                    About
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="">Pages</NavLink>
+                </li>
+                <li>
+                  <NavLink to="">Blog</NavLink>
+                </li>
+                <li>
+                  <NavLink to="">Gallery</NavLink>
+                </li>
+                <li>
+                  <NavLink to="">Contact</NavLink>
+                </li>
+              </ul>
+            </nav>
+            <footer>
+              <div class="social">
+                <NavLink to="">
+                  <img src="images/facebook.png" alt="" />
                 </NavLink>
-              </li>
-              <li>
-                <NavLink to="">Pages</NavLink>
-              </li>
-              <li>
-                <NavLink to="">Blog</NavLink>
-              </li>
-              <li>
-                <NavLink to="">Gallery</NavLink>
-              </li>
-              <li>
-                <NavLink to="">Contact</NavLink>
-              </li>
-            </ul>
-          </nav>
-          <footer>
-            <div class="social">
-              <NavLink to="">
-                <img src="images/facebook.png" alt="" />
-              </NavLink>
-              <NavLink to="">
-                <img src="images/twitter.png" alt="" />
-              </NavLink>
-              <NavLink to="">
-                <img src="images/instagram.png" alt="" />
-              </NavLink>
-            </div>
-            <p>2022, Made with love by Supatarachai kwandee</p>
-          </footer>
-        </div>
-      </header>
+                <NavLink to="">
+                  <img src="images/twitter.png" alt="" />
+                </NavLink>
+                <NavLink to="">
+                  <img src="images/instagram.png" alt="" />
+                </NavLink>
+              </div>
+              <p>2022, Made with love by Supatarachai kwandee</p>
+            </footer>
+          </div>
+        </header>
+      </div>
+      <Profile />
     </div>
   );
 }
---
+++ src/components/contents/Profile.js
@@ -0,0 +1,58 @@
+import React, { useEffect } from "react";
+import "../../styles/profile.css";
+import WOW from "wowjs";
+
+export default function Profile() {
+  useEffect(() => {
+    new WOW.WOW({
+      live: false,
+    }).init();
+  }, []);
+  return (
+    <div class="home container text-white" id="home">
+      <div class="row">
+        <div
+          class="col-md-6 wow fadeInUp"
+          data-wow-duration="1s"
+          data-wow-delay="1s"
+        >
+          <div class="hero-text">
+            <span class="title">Hello i'm</span>
+            <br />
+            <span class="name">KWANDEE S.</span> <br />
+            <span class="designation">FrontEnd Web Designer</span> <br />
+            <span class="email">
+              <i class="fa fa-envelope"></i> kwandee1992@gmail.com
+            </span>
+            <br />
+            <span class="phone">
+              <i class="fa fa-phone"></i> 098-968-4958
+            </span>
+            <div class="social-icons d-flex">
+              <li>
+                <i class="fa fa-facebook"></i>
+              </li>
+              <li>
+                <i class="fa fa-twitter"></i>
+              </li>
+              <li>
+                <i class="fa fa-linkedin"></i>
+              </li>
+              <li>
+                <i class="fa fa-instagram"></i>
+              </li>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-6 wow fadeInUp">
+          <div class="hero-img">
+            <div class="img-border">
+              <img src="/me.jpg" class="img-fluid" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
